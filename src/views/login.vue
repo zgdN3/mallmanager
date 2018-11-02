9c284a15534b6b5f1c6ff1753abdfcc6a56cbd98
zgdN3 <wangzijie1031@qq.com>
2018-11-02 08:23:17 +0800

    :tada: Initial commit

---
+++ src/views/login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <el-form label-position="top" label-width="80px" :model="formData">
+      <el-form-item label="用户名">
+        <el-input v-model="formData.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="formData.password"></el-input>
+      </el-form-item>
+      <el-button type="primary">主要按钮</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
